bb81c63eb21187ae293971dddd84d467ad64b530
Glitch (speedrun) <none>
2018-02-21 06:37:09 +0000

    👡💼 Checkpoint s/main.js:250123/100 server.js:250123/211

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.static('s'));
 
+app.get('/https://www.speedrun.com/api/', (req, res) => {
+  // forward request to https://www.speedrun.com/api/, and cache locally!
+});
+
+app.use((req, res) => {
+  res.sendFile(__dirname + '/s/index.html');
+})
+
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
 });
