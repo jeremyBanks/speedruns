f3ec9c65b3f47ba7944c5c88868543d2ef9fb3af
Glitch (speedrun) <none>
2018-02-21 05:53:09 +0000

    🙏⛩ Checkpoint s/main.js:250123/1128 s/index.html:250123/27 s/style.css:250123/81

--- s/main.js
+++ s/main.js
@@ -1,11 +1,21 @@
 export const main = async () => {
+  await 0;
+
   result.then(
-    _ => document.querySelector('#main-loading').remove(),
-    error => document.querySelector('#main-loading').textContent = String(error));
+    _ => document.querySelector('#premain-message').remove(),
+    error => document.querySelector('#premain-message').textContent = String(error));
 
   const output = document.querySelector('#main');
+
+  const {hostname} = document.location;
+  const projectName = hostname.matches(/^[a-z0-9\-]+\.glitch\.io/) ? hostname.split('.')[0] : null;
+  const heading = Object.assign(document.createElement('h1'), {textContent: `${projectName || 'speedrun'}.glitch.io`});
   
-  output.foo.bar;
+  if (projectName) {
+    heading.appendChild(document.createTextNode(' ('));
+    heading.appendChild(Object.assign(document.createElement('a'), {href: `https://glitch.com/edit/#!/${projectName}`}));
+    heading.appendChild(document.createTextNode(')'));
+  }
 };
 
-const result = Promise.resolve().then(main);
+const result = main();
