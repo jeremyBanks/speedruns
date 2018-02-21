ae6bc68c60720fd0b2c2599dd73ccbda6a65dfcb
Jeremy <_@jeremy.ca>
2018-02-21 05:56:16 +0000

    r4 charlie eight alfa five

--- s/main.js
+++ s/main.js
@@ -1,21 +1,36 @@
 export const main = async () => {
   await 0;
 
-  result.then(
-    _ => document.querySelector('#premain-message').remove(),
-    error => document.querySelector('#premain-message').textContent = String(error));
+  onLoad(main.result);
 
   const output = document.querySelector('#main');
 
-  const {hostname} = document.location;
-  const projectName = hostname.matches(/^[a-z0-9\-]+\.glitch\.io/) ? hostname.split('.')[0] : null;
-  const heading = Object.assign(document.createElement('h1'), {textContent: `${projectName || 'speedrun'}.glitch.io`});
-  
+  const hostname = document.location.host;
+  const projectName = hostname.match(/^[a-z0-9\-]+\.glitch\.me$/) ? hostname.split('.')[0] : null;
+  const title = `${projectName || 'speedrun'}.glitch.me`;
+
+  document.title = title;
+
+  const heading = Object.assign(document.createElement('h1'), {textContent: title});
   if (projectName) {
     heading.appendChild(document.createTextNode(' ('));
-    heading.appendChild(Object.assign(document.createElement('a'), {href: `https://glitch.com/edit/#!/${projectName}`}));
+    heading.appendChild(Object.assign(document.createElement('a'), {
+      href: `https://glitch.com/edit/#!/${projectName}`,
+      textContent: 'view source'}));
     heading.appendChild(document.createTextNode(')'));
   }
+  output.appendChild(heading);
+
+  hello.world;
 };
 
-const result = main();
+const cleanUp = async result => {
+  result.then(
+    _ => document.querySelector('#premain-message').remove(),
+    error => {
+      document.querySelector('#premain-message').textContent = String(error.stack);
+      throw error;
+    });
+};
+
+main.result = main();
