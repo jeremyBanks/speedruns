2f08f1d415118ec24e89178e764581507f81b097
Jeremy <_@jeremy.ca>
2018-02-21 18:52:48 +0000

    r22 five bravo zero one

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const request = require('request-promise-native');
 
 const app = express();
 
+// Serve static files from s/.
 app.use(express.static('s'));
 
+// Crudely mirror and cache speedrun.com/api.
+// We never expire/evict values here; we assume
+// the process won't live long enough for this to get very stale.
 const apiCache = new Map();
 app.get(/^\/(https:\/\/www\.speedrun\.com\/api\/(.*))/, async (req, res) => {
   const url = req.url.slice(1);
   const cached = apiCache.get(url);
   
   if (cached) {
-    return await cached;
+    return res.send(await cached);
   }
 
   console.log("GETting", url);
@@ -21,6 +25,7 @@ app.get(/^\/(https:\/\/www\.speedrun\.com\/api\/(.*))/, async (req, res) => {
   return res.send(await result);
 });
 
+// Serve index for unknown URLs so it can route them client-side.
 app.use((req, res) => {
   res.sendFile(__dirname + '/s/index.html');
 });
