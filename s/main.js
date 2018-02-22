7d5c2b653c502081a54dac509c99ce0a3b97174c
Glitch (bests) <none>
2018-02-22 04:17:25 +0000

    👶🏵 Checkpoint s/main.js:250123/439

--- s/main.js
+++ s/main.js
@@ -53,77 +53,80 @@ import HTML from '/html.js';
     if (!gamesSlug) throw new Error("no game(s) in URL");
     if (!playerSlug) throw new Error("no player in URL");
 
-    const gameSlugs = gamesSlug.split(/\+/g)    
+    const gameSlugs = gamesSlug.split(/\+/g).filter(Boolean);
+    if (gameSlugs.length == 0) throw new Error("no game(s) in URL");
 
     const [
       playerInfo,
-      gameInfo,
+      ...gameInfos
     ] = await Promise.all([
-      apiFetch(`games/${gameSlug}?embed=levels,categories`),
       apiFetch(`users/${playerSlug}`),
-    ]);
+    ].concat(gameSlugs.map(
+      gameSlug => apiFetch(`games/${gameSlug}?embed=levels,categories`))));
 
-    const gameId = gameInfo.id;
-    const playerId = gameInfo.id;
+    for (const gameInfo of gameInfos) {
+      const gameId = gameInfo.id;
+      const playerId = gameInfo.id;
 
-    const gameName = gameInfo.names.international;
-    const playerName = playerInfo.names.international;
+      const gameName = gameInfo.names.international;
+      const playerName = playerInfo.names.international;
 
-    const runsInfo = await apiFetch(`runs?user=${playerId}&game=${gameId}`);
+      const runsInfo = await apiFetch(`runs?user=${playerId}&game=${gameId}`);
 
-    const icon = gameInfo.assets.icon.uri;
-    const [gold, silver, bronze] = ['trophy-1st', 'trophy-2nd', 'trophy-3rd'].map(s => gameInfo.assets[s].uri);
+      const icon = gameInfo.assets.icon.uri;
+      const [gold, silver, bronze] = ['trophy-1st', 'trophy-2nd', 'trophy-3rd'].map(s => gameInfo.assets[s].uri);
 
-    renderHTML`
-      <h2>${gameName}</h2>
+      renderHTML`
+        <h2>${gameName}</h2>
 
-      <h3><img src="${icon}"> Full Game <img src="${icon}"></h3>
+        <h3><img src="${icon}"> Full Game <img src="${icon}"></h3>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Category</th>
-            <th>World Record</th>
-            <th>${playerName}'s Best</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>All Campaigns</th>
-            <td>
-              -
-            </td>
-            <td>-</td>
-          </tr>
-          <tr>
-            <th>Orc Campaign</th>
-            <td>
-              <img src="${gold}"> 4h 2m 30s <br>
-              by John Smith
-            </td>
-            <td>
-              <img src="${silver}"> 6h 22m 13s
-            </td>
-          </tr>
-          <tr>
-            <th>Human Campaign</th>
-            <td>
-              <img src="${gold}"> 10m 13s <br>
-              by John Smith
-            </td>
-            <td>-</td>
-          </tr>
-        </tbody>
-      </table>
+        <table>
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>World Record</th>
+              <th>${playerName}'s Best</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>All Campaigns</th>
+              <td>
+                -
+              </td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <th>Orc Campaign</th>
+              <td>
+                <img src="${gold}"> 4h 2m 30s <br>
+                by John Smith
+              </td>
+              <td>
+                <img src="${silver}"> 6h 22m 13s
+              </td>
+            </tr>
+            <tr>
+              <th>Human Campaign</th>
+              <td>
+                <img src="${gold}"> 10m 13s <br>
+                by John Smith
+              </td>
+              <td>-</td>
+            </tr>
+          </tbody>
+        </table>
 
-      <h3><img src="${icon}"> Individual Levels <img src="${icon}"></h3>
+        <h3><img src="${icon}"> Individual Levels <img src="${icon}"></h3>
 
-      <p>foo</p>
+        <p>foo</p>
 
-      <pre>${JSON.stringify(gameInfo, null, 2)}</pre>
-      <pre>${JSON.stringify(playerInfo, null, 2)}</pre>
-      <pre>${JSON.stringify(runsInfo, null, 2)}</pre>
-    `;
+        <pre>${JSON.stringify(gameInfo, null, 2)}</pre>
+        <pre>${JSON.stringify(playerInfo, null, 2)}</pre>
+        <pre>${JSON.stringify(runsInfo, null, 2)}</pre>
+      `;
+    }
   }
 
   renderHTML`
