138393efec22a2d860631431bab78e334256eb6b
Jeremy <_@jeremy.ca>
2018-04-09 23:45:04 +0000

    r191 alfa three foxtrot echo

---
+++ src/extra-runs.js
@@ -0,0 +1,13 @@
+class UnofficialRun {
+  constructor(props) {
+    this.weblink = ;
+    Object.seal(this);
+    Object.assign(this, props);
+  }
+}
+
+export const extraData = {
+  'runs?game=o1yry26q&category=wdmw5ee2&level=xd17ejqd&status=verified&orderby=date&direction=asc': [
+    
+  ]
+};
