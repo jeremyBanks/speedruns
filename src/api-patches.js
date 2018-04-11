5745350cbff1d6c9b90024397237051d9c5117cb
Glitch (bests) <none>
2018-04-11 00:27:11 +0000

    🏫🏓 Checkpoint ./src/api-patches.js:250123/2935

--- src/api-patches.js
+++ src/api-patches.js
@@ -105,42 +105,55 @@ export const extraData = {
     run('AverageAvocado', 208, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'n93750nd')]: [ // human2
+    run('AverageAvocado', 76, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 89, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'z986vjgd')]: [ // human3
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 409, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'rdn0j5qw')]: [ // human4
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 710, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'ldyy7ekd')]: [ // human5
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 966, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'gdre4j69')]: [ // human6
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 927, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'nwllx60w')]: [ // human7
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 1008, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'ywe1k4yd')]: [ // human8
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 918, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, '69z3orld')]: [ // human9
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 503, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'r9g20z5d')]: [ // human10
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 1002, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'o9xn520w')]: [ // human11
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 1037, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, '4956oj0d')]: [ // human12
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 1180, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, 'rdqj20k9')]: [ // human13
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 2337, _.avoUrl2015, _.avoDate2015),
   ],
   [runs(war2, war2Mission, '5d7vk7gd')]: [ // human14
+    run('AverageAvocado', 911911, _.avoUrlHuman2013, _.avoDate2013),
     run('AverageAvocado', 1462, _.avoUrl2015, _.avoDate2015),
   ],
 
@@ -148,26 +161,37 @@ export const extraData = {
     run('AverageAvocado', 411, _.avoUrlXHuman2013 = 'https://www.youtube.com/playlist?list=PL5b0fctdcCFMrfEc2a3oI_25cPHwmccxG', _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'rdn0jpqw')]: [ // xhuman2
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'ldyy7pkd')]: [ // xhuman3
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'gdre4q69')]: [ // xhuman4
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'nwllx70w')]: [ // xhuman5
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'ywe1k8yd')]: [ // xhuman6
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, '69z3o4ld')]: [ // xhuman7
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'r9g2035d')]: [ // xhuman8
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'o9xn5l0w')]: [ // xhuman9
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, '4956o80d')]: [ // xhuman10
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, 'rdqj21k9')]: [ // xhuman11
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
   [runs(war2x, war2xMission, '5d7vk3gd')]: [ // xhuman12
+    run('AverageAvocado', 911911, _.avoUrlXHuman2013, _.avoDate2013),
   ],
 
   [runs(war2x, war2xMission, 'kwj5lyn9')]: [ // xorc1
@@ -175,36 +199,47 @@ export const extraData = {
     run('AverageAvocado', 284, 'https://youtu.be/eeBgiR8OBlU?t=66', '2015-05-15'),
   ],
   [runs(war2x, war2xMission, 'owo01kow')]: [ // xorc2
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 741, 'https://youtu.be/5YjjECd8LMg?t=45', '2015-05-16'),
   ],
   [runs(war2x, war2xMission, 'xd17e0ed')]: [ // xorc3
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 951, 'https://youtu.be/0AnFa52gul0?t=39', '2015-05-22'),
   ],
   [runs(war2x, war2xMission, 'ewpjmqkw')]: [ // xorc4
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 860, 'https://youtu.be/AdtTlUA4sa0?t=56', '2015-05-23'),
   ],
   [runs(war2x, war2xMission, 'y9mjqp59')]: [ // xorc5
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 1039, 'https://youtu.be/mOsxBWDLGjM?t=62', '2015-05-29'),
   ],
   [runs(war2x, war2xMission, '5wkjzr2d')]: [ // xorc6
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 1373, 'https://youtu.be/W5I7WNnARpc?t=47', '2015-05-30'),
   ],
   [runs(war2x, war2xMission, '592zgoo9')]: [ // xorc7
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 1983, 'https://youtu.be/XU5rZn8VNP0?t=62', '2015-06-05'),
   ],
   [runs(war2x, war2xMission, '29v3ek3w')]: [ // xorc8
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 1771, 'https://youtu.be/IoVa94tVuzA?t=36', '2015-06-06'),
   ],
   [runs(war2x, war2xMission, 'xd4j732d')]: [ // xorc9
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 578, 'https://youtu.be/KNKMXghZbnY?t=62', '2015-06-12'),
   ],
   [runs(war2x, war2xMission, 'xd0gr7xw')]: [ // xorc10
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 1333, 'https://youtu.be/VjWPkHj3qio?t=66', '2015-06-12'),
   ],
   [runs(war2x, war2xMission, 'rw64pnr9')]: [ // xorc11
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 1721, 'https://youtu.be/10awDV6v9t0?t=51', '2015-06-16'),
   ],
   [runs(war2x, war2xMission, 'n9375gnd')]: [ // xorc12
+    run('AverageAvocado', 911911, _.avoUrlXOrc2013, _.avoDate2013),
     run('AverageAvocado', 2307, 'https://youtu.be/tUgBU_3yO6s?t=54', '2015-06-20'),
   ],
 };
