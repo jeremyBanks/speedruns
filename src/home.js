e46d74a60c43171b6f0be49c02847d6e64750e85
Jeremy <_@jeremy.ca>
2018-04-28 18:44:16 +0000

    r444 four charlie six five

--- src/home.js
+++ src/home.js
@@ -19,13 +19,14 @@ export class HomeBody extends Component {
     return HTML`
       <p>This site compares personal and world record speed run progress over time.</p>
 
-<pre>
+<pre ${style({line: {height: 1.5}})}>
                      <u>Example URLs</u>
-<span ${this.labelStyle}>        runner view:</span> ${link({runnerSlugs: ['banks']})}
-<span ${this.labelStyle}>          game view:</span> ${link({gameSlugs: ['wc2']})}
-<span ${this.labelStyle}>   + multiple games:</span> ${link({gameSlugs: ['wc2', 'wc2btdp']})}
-<span ${this.labelStyle}>   + personal bests:</span> ${link({gameSlugs: ['wc2', 'wc2btdp'], runnerSlugs: ['ZPR']})}
-<span ${this.labelStyle}>         level view:</span> ${link({gameSlugs: ['smwext'], levelSlugs: ['world-1'], runnerSlugs: ['banks', 'lui']})}
+<span ${this.labelStyle}>             runner:</span> ${link({runnerSlugs: ['banks']})}
+<span ${this.labelStyle}>               game:</span> ${link({gameSlugs: ['wc2']})}
+<span ${this.labelStyle}>     multiple games:</span> ${link({gameSlugs: ['wc2', 'wc2btdp']})}
+<span ${this.labelStyle}>with personal bests:</span> ${link({gameSlugs: ['wc2', 'wc2btdp'], runnerSlugs: ['ZPR']})}
+<span ${this.labelStyle}>              level:</span> ${link({gameSlugs: ['smwext'], levelSlugs: ['world-1'], runnerSlugs: ['banks', 'lui']})}
+<span ${this.labelStyle}>          using IDs:</span> ${link({gameSlugs: ['268n5y6p'], levelSlugs: ['9kvpep8k'], runnerSlugs: ['18qyezox', '7j4q22dx']})}
 </pre>
 
     `;
