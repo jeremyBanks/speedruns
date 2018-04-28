faae9af39de3d49f32020e7897c2921c642f5f3a
Glitch (bests) <none>
2018-04-28 01:15:02 +0000

    🐇👛 Checkpoint ./src/home.js:250123/1490 ./src/index.html:250123/42

--- src/home.js
+++ src/home.js
@@ -4,26 +4,33 @@ import {style} from '/assets/bester/style.js';
 
 
 export class HomeBody extends Component {
+  get urlStyle() {
+    return style({
+      text: {decoration: 'underline'}
+    });
+  }
+
   render({}) {
     return HTML`
       <p>This site lets you compare personal and world record progressions over time.</p>
 
-      <p>
-        Example URLs:
-      </p>
-
 <pre>
-      
-      <ul>
-        <li><code><a href="/@banks">/@banks</a></code></li>
-        <li><code><a href="/wc2+wc2btdp/@zpr">/wc2+wc2btdp/@zpr</a></code></li>
-        <li><code><a href="/smwext/world-1/@banks">/smwext/world-1/@banks</a></code></li>
-        <li><code><a href="/smwext/@QuiteSuperMario#level-xd1rnr7k">/smwext/@QuiteSuperMario#level-xd1rnr7k</a></code></li>
-      </ul>
+                     <u>Example URLs</u>
+        Player View: <a ${this.urlStyle} href="/@banks">/@banks</a>
+          Game View: <a ${this.urlStyle} href="/wc2">/sc1</a>
+   + Multiple Games: <a ${this.urlStyle} href="/wc2+wc2btdp">/wc2+wc2btdp</a>
+   + Personal Bests: <a ${this.urlStyle} href="/wc2+wc2btdp/@zpr">/wc2+wc2btdp/@zpr</a>
+         Level View: <a ${this.urlStyle} href="/smwext/world-1/@banks+@lui">/smwext/world-1/@banks+@lui</a>
+</pre>
+
     `;
   }
   
   get style() {
-    return style({text: {align: 'left'}});
+    return style({
+      display: 'block',
+      margin: {top: '16px', bottom: '32px'},
+      text: {align: 'left'},
+    });
   }
 }
