ba489b1186193a13137253e35a3da4273f525e83
Jeremy <_@jeremy.ca>
2018-10-10 05:45:36 +0000

    r7 zero echo charlie delta

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use serde_json;
 extern crate serde_derive;
 #[macro_use]
 extern crate derive_more;
+use itertools::Itertools;
 
 use std::{collections::BTreeMap, convert::From, error::Error, fs};
 
@@ -42,25 +43,15 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     let data = data_source::SpeedRunComData::open("data.json");
 
-    let war2 = data
-        .games()
-        .get("o1yry26q")
-        .expect("war2 should be in our data");
-    let war2x = data
-        .games()
-        .get("y65zy46e")
-        .expect("war2x should be in our data");
-
+    let war2 = &data.games()["o1yry26q"];
     let level = &war2.levels[0];
-    let some_level_id = Some(level.level_id.to_string());
-
-    let mut level_runs: Vec<_> = data
+    let runs_by_level = data
         .runs()
         .values()
-        .filter(|run| run.level_id == some_level_id)
-        .collect();
-
-    level_runs.sort_by(|a, b| {
+        .map(|run| (run.level_id.clone(), run))
+        .into_group_map();
+    let mut runs = runs_by_level[&Some(level.level_id.clone())].clone();
+    runs.sort_by(|a, b| {
         a.duration
             .partial_cmp(&b.duration)
             .expect("no NaN here")
@@ -69,7 +60,7 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     });
 
     println!("First level: {:#?}", level);
-    println!("World record: {:#?}", level_runs[0]);
+    println!("World record: {:#?}", &runs[0..4]);
 
     Ok(())
 }
