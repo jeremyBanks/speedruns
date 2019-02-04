fe05c921de51933a0a3eded9e39a431c9478518a
Jeremy <_@jeremy.ca>
2019-02-04 13:20:00 +0000

    r4 eight zero eight echo

--- src/bin/structure.rs
+++ src/bin/structure.rs
@@ -25,43 +25,43 @@ fn main() -> Result<!, DynError> {
         env_logger::Env::default().filter_or("RUST_LOG", "structure=trace"),
     )?;
 
-    let file = File::open("data/games.jsonl.gz")?;
-    let buffer = BufReader::new(&file);
-    let decompressor = GzDecoder::new(buffer);
-    let deserializer = JsonDeserializer::from_reader(decompressor);
-    let iterator = deserializer.into_iter::<JsonValue>();
-    for data in iterator {
-        let data = data?;
-        let game_result = api_types::Game::deserialize(&data);
-        if let Err(err) = game_result {
-            error!(
-                "{:#?} deserializing {}",
-                err,
-                serde_json::to_string(&data).unwrap()
-            );
-            return Err(err.into());
-        }
-    }
-    info!("Deserialized all games.");
+    // let file = File::open("data/games.jsonl.gz")?;
+    // let buffer = BufReader::new(&file);
+    // let decompressor = GzDecoder::new(buffer);
+    // let deserializer = JsonDeserializer::from_reader(decompressor);
+    // let iterator = deserializer.into_iter::<JsonValue>();
+    // for data in iterator {
+    //     let data = data?;
+    //     let game_result = api_types::Game::deserialize(&data);
+    //     if let Err(err) = game_result {
+    //         error!(
+    //             "{:#?} deserializing {}",
+    //             err,
+    //             serde_json::to_string(&data).unwrap()
+    //         );
+    //         return Err(err.into());
+    //     }
+    // }
+    // info!("Deserialized all games.");
 
-    let file = File::open("data/users.jsonl.gz")?;
-    let buffer = BufReader::new(&file);
-    let decompressor = GzDecoder::new(buffer);
-    let deserializer = JsonDeserializer::from_reader(decompressor);
-    let iterator = deserializer.into_iter::<JsonValue>();
-    for data in iterator {
-        let data = data?;
-        let user_result = api_types::User::deserialize(&data);
-        if let Err(err) = user_result {
-            error!(
-                "{:#?} deserializing {}",
-                err,
-                serde_json::to_string(&data).unwrap()
-            );
-            return Err(err.into());
-        }
-    }
-    info!("Deserialized all users.");
+    // let file = File::open("data/users.jsonl.gz")?;
+    // let buffer = BufReader::new(&file);
+    // let decompressor = GzDecoder::new(buffer);
+    // let deserializer = JsonDeserializer::from_reader(decompressor);
+    // let iterator = deserializer.into_iter::<JsonValue>();
+    // for data in iterator {
+    //     let data = data?;
+    //     let user_result = api_types::User::deserialize(&data);
+    //     if let Err(err) = user_result {
+    //         error!(
+    //             "{:#?} deserializing {}",
+    //             err,
+    //             serde_json::to_string(&data).unwrap()
+    //         );
+    //         return Err(err.into());
+    //     }
+    // }
+    // info!("Deserialized all users.");
 
     let file = File::open("data/run.jsonl.gz")?;
     let buffer = BufReader::new(&file);
