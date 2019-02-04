007543a24c22d02ae1eea841a2d0a6a8421ac197
Jeremy <_@jeremy.ca>
2019-02-04 13:20:00 +0000

    r62 one charlie foxtrot bravo

--- build.rs
+++ build.rs
@@ -1,16 +1,26 @@
+use std::{fs, io};
+
 fn main() {
-    for name in &[
-        "runs",
-        "users",
-        "games",
-        "categories",
-        "levels",
-    ] {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    fs::create_dir_all(&format!("{}/data/normalized", out_dir))
+        .expect("unable to create normalized data directory");
+
+    for name in &["runs", "users", "games", "categories", "levels"] {
         let path = format!("data/normalized/{}.bin.xz", name);
-        std::fs::OpenOptions::new()
+        let out_path = format!("{}/{}", out_dir, path);
+
+        let mut out_file = fs::OpenOptions::new()
             .append(true)
             .create(true)
-            .open(&path)
-            .expect("unable to create placeholder data file");
+            .open(&out_path)
+            .expect("unable to open/create normalized data file");
+
+        if let Ok(mut source_file) =
+            fs::OpenOptions::new().read(true).open(&path)
+        {
+            io::copy(&mut source_file, &mut out_file)
+                .expect("failed to copy normalized data");
+        }
     }
 }
