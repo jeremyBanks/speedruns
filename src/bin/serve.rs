c1974ce2e38488c57f1715756a2539996d25044a
Jeremy Banks <_@jeremy.ca>
2019-12-29 03:08:09 -0500

    --allow-empty-message

--- src/bin/serve.rs
+++ src/bin/serve.rs
@@ -1,4 +1,9 @@
-#![allow(missing_docs, clippy::useless_attribute, clippy::useless_vec)]
+#![allow(
+    missing_docs,
+    clippy::useless_attribute,
+    clippy::useless_vec,
+    unused_imports
+)]
 #![warn(missing_debug_implementations)]
 #![deny(unconditional_recursion)]
 
@@ -16,7 +21,7 @@ use std::{convert::Infallible, sync::Arc};
 
 pub mod schema {
     use juniper::{FieldResult, RootNode};
-    // juniper macro components
+
     #[allow(unused)]
     use juniper::{
         graphql_interface, graphql_object, graphql_scalar, graphql_union, graphql_value,
@@ -24,32 +29,22 @@ pub mod schema {
         ScalarValue,
     };
 
-    #[derive(Debug, Clone)]
+    #[derive(Debug)]
     pub struct Context {}
     impl juniper::Context for Context {}
 
-    #[derive(GraphQLEnum)]
+    #[derive(Debug, GraphQLEnum)]
     enum Episode {
         NewHope,
         Empire,
         Jedi,
     }
 
-    #[derive(GraphQLObject)]
-    #[graphql(description = "A humanoid creature in the Star Wars universe")]
-    struct Human {
-        id:          String,
-        name:        String,
-        appears_in:  Vec<Episode>,
-        home_planet: String,
-    }
-
-    #[derive(GraphQLInputObject)]
-    #[graphql(description = "A humanoid creature in the Star Wars universe")]
-    struct NewHuman {
-        name:        String,
-        appears_in:  Vec<Episode>,
-        home_planet: String,
+    #[derive(Debug, GraphQLObject)]
+    #[graphql(description = "A user")]
+    pub struct User {
+        id:   String,
+        name: String,
     }
 
     #[derive(Debug, Default)]
@@ -57,12 +52,14 @@ pub mod schema {
 
     #[juniper::object(Context = Context)]
     impl Query {
-        pub fn human(context: &Context, id: String) -> FieldResult<Human> {
-            Ok(Human {
-                id:          "1234".to_owned(),
-                name:        "Luke".to_owned(),
-                appears_in:  vec![Episode::NewHope],
-                home_planet: "Mars".to_owned(),
+        pub fn user(
+            context: &Context,
+            id: Option<String>,
+            name: Option<String>,
+        ) -> FieldResult<User> {
+            Ok(User {
+                id:   id.unwrap_or_else(|| "123".to_string()),
+                name: name.unwrap_or_else(|| "Nom".to_string()),
             })
         }
     }
@@ -81,7 +78,7 @@ pub mod schema {
 }
 
 async fn graphiql() -> actix_web::HttpResponse {
-    let html = graphiql_source("/graphql");
+    let html = juniper::http::graphiql::graphiql_source("/graphql");
     actix_web::HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html)
@@ -90,7 +87,7 @@ async fn graphiql() -> actix_web::HttpResponse {
 async fn graphql(
     st: web::Data<Arc<schema::Schema>>,
     data: web::Json<GraphQLRequest>,
-) -> Result<actix_web::HttpResponse, actix_web::Error> {
+) -> actix_web::Result<actix_web::HttpResponse> {
     let user = web::block(move || {
         let res = data.execute(&st, &schema::Context {});
         Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
@@ -103,23 +100,21 @@ async fn graphql(
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    // Enable all debug logs by default.
     if std::env::var("RUST_LOG").unwrap_or_default().is_empty() {
         std::env::set_var("RUST_LOG", "debug");
     }
     pretty_env_logger::init();
 
-    // Create Juniper schema
-    let schema = std::sync::Arc::new(schema::new());
+    let schema = Arc::new(schema::new());
 
-    // Start http server
-    actix_web::HttpServer::new(move || {
+    let server = actix_web::HttpServer::new(move || {
         actix_web::App::new()
             .data(schema.clone())
             .wrap(actix_web::middleware::Logger::default())
             .service(web::resource("/graphql").route(web::post().to(graphql)))
-            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+            .service(web::resource("/{_:(graphiql)?}").route(web::get().to(graphiql)))
+    });
+
+    server.bind("127.0.0.1:8080")?.run().await
 }
