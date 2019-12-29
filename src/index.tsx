9d16ba3f82a24d167713a7c9fc7906f949fd7052
Jeremy Banks <_@jeremy.ca>
2019-12-29 13:56:42 -0500

    cra

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+  return <div>hello world</div>;
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
