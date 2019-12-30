cfab885fdf46f8e5211fce12c8a93a697479b38d
Jeremy Banks <_@jeremy.ca>
2019-12-29 21:32:24 -0500

    lazily add a new query definition

--- src/data/graphql.rs
+++ src/data/graphql.rs
@@ -31,12 +31,13 @@ pub struct Query {}
 
 #[juniper::object(Context = Context)]
 impl Query {
-    /// Get a user by id or slug. throws an error if none are specified, or no user matches
-    /// all that are specified.
+    #[graphql( description = "
+        Get a user by id or slug. Throws an error if none are specified,
+        or no user matches all that are specified.")]
     pub fn user(
         context: &Context,
         id: Option<String>,
-        name: Option<String>,
+         name: Option<String>,
     ) -> FieldResult<User> {
         Err(FieldError::from("not implemented"))
     }
