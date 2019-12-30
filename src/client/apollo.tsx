41bbc4f4fbf8942c9d7edbf086b659ec6617a1ef
Jeremy <_@jeremy.ca>
2019-12-30 02:46:24 +0000

    r118 six four zero four

--- src/client/apollo.tsx
+++ src/client/apollo.tsx
@@ -4,7 +4,6 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 
-import { TypeNames } from "./__generated__/TypeNames";
 import { GetUserById } from "./__generated__/GetUserById";
 
 export const client = new ApolloClient({
@@ -26,14 +25,3 @@ export const useGetUserById = (id: String) =>
     `,
     { variables: { id } }
   );
-
-export const useTypeNames = () =>
-  useQuery<TypeNames>(gql`
-    query TypeNames {
-      __schema {
-        types {
-          name
-        }
-      }
-    }
-  `);
