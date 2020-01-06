aed5ca6f91aad9181aeef72315f8059b7e71beda
Jeremy <_@jeremy.ca>
2020-01-06 18:10:08 +0000

    r272 zero five zero foxtrot

--- src/pages-lib/global.d.ts
+++ src/pages-lib/global.d.ts
@@ -2,3 +2,10 @@ declare module "*.scss" {
   export const content: { [className: string]: string };
   export default content;
 }
+
+declare module "graphiql" {
+  export const GraphiQL: React.FC<{
+    fetcher: (query: unknown) => Promise<any>;
+  }>;
+  export default GraphiQL;
+}
