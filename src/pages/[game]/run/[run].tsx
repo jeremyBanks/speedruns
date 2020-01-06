dd427b8794f2f44d43cfaed4ef33844eb2639492
Jeremy <_@jeremy.ca>
2020-01-06 01:06:08 +0000

    r261 one eight three foxtrot

--- src/pages/[game]/run/[run].tsx
+++ src/pages/[game]/run/[run].tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { NextPage } from "next";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
-const RunPage: NextPage = () => {
+import { withApollo } from "../../../pages-lib/with-apollo";
+import * as schema from "../../../pages-lib/schema";
+
+const RunPage: NextPage<{}> = () => {
   const router = useRouter();
+  const { loading, error, data } = useQuery<schema.GetRun>(GetRun, {
+    variables: { id: router.query.run }
+  });
 
-  return (
-    <p>
-      game: {router.query.game}, run: {router.query.run}
-    </p>
-  );
+  if (data && !error) {
+    return <p>{JSON.stringify(data)}</p>;
+  } else {
+    return <p>loading: {String(loading || error)}</p>;
+  }
 };
 
-export default RunPage;
+export default withApollo(RunPage);
+
+const GetRun = gql`
+  query GetRun($id: String!) {
+    run(id: $id) {
+      id
+    }
+  }
+`;
