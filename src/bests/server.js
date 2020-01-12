1a3718cfd32a50bb2ba6adaf6aa35d1677817113
Jeremy Banks <_@jeremy.ca>
2020-01-11 22:29:36 -0500

    actually fix bests for running locally

--- src/bests/server.js
+++ src/bests/server.js
@@ -1,58 +1,64 @@
-import express from 'express';
-import compression from 'compression';
-import rp from 'request-promise-native';
-import serveIndex from 'serve-index';
-import commonmark from 'commonmark';
+import express from "express";
+import compression from "compression";
+import rp from "request-promise-native";
+import serveIndex from "serve-index";
+import commonmark from "commonmark";
 
-import url from 'url';
-
+import url from "url";
 
 // We don't use this yet, but don't want to break it.
-import * as speedrun from '/assets/speedrun.js';
-import HTML from '/assets/bester/html.js';
-
+import * as speedrun from "/assets/speedrun.js";
+import HTML from "/assets/bester/html.js";
 
 const app = express();
 
 // enable strong etags
-app.set('etag', 'strong');
+app.set("etag", "strong");
 
 // enable compression
 app.use(compression());
 
 // disable json pretty print
-app.set('json spaces', 2);
-
+app.set("json spaces", 2);
 
 // Hard-coded until we have an ES-module-friendly alternative,
 // such as https://github.com/tc39/proposal-import-meta
-const __dirname = '/app';
-
+const __dirname = "./";
 
 // We serve all static files under the public path 'assets/' because that's what speedrun.com
 // uses, so by copying that we can avoid clobbering any of their paths we'd like to mirror.
 
 // Serve directory indexes for public/ftp folder (with icons)
-app.use('/assets', serveIndex(__dirname + '/src', {
-  view: 'details',
-  icons: true,
-}));
-app.use('/assets', express.static(__dirname + '/src', {
-  dotfiles: 'ignore',
-  index: ['index.html', 'README.md']
-}));
+app.use(
+  "/assets",
+  serveIndex(__dirname + "/src", {
+    view: "details",
+    icons: true,
+  }),
+);
+app.use(
+  "/assets",
+  express.static(__dirname + "/src", {
+    dotfiles: "ignore",
+    index: ["index.html", "README.md"],
+  }),
+);
 
-
-app.get('/*.md', async (req, res, next) => {
-  const input = await new Promise((resolve, reject) => fs.readFile(__dirname + req.url, 'utf8', (err, data) => { err ? reject(err) : resolve(data); }));
+app.get("/*.md", async (req, res, next) => {
+  const input = await new Promise((resolve, reject) =>
+    fs.readFile(__dirname + req.url, "utf8", (err, data) => {
+      err ? reject(err) : resolve(data);
+    }),
+  );
   const reader = new commonmark.Parser();
-  const writer = new commonmark.HtmlRenderer({safe: true});
+  const writer = new commonmark.HtmlRenderer({ safe: true });
   const result = writer.render(reader.parse(input));
-  const projectName = process.env.PROJECT_NAME || 'bests';
-  
-  res.set('Content-Type', 'text/html');
-          
-  res.send(await HTML.string`<!doctype html>
+  const projectName = process.env.PROJECT_NAME || "bests";
+
+  res.set("Content-Type", "text/html");
+
+  res.send(
+    await HTML.string`<!doctype html>
 <html>
 <head>
 <title>${req.url}</title>
@@ -76,29 +82,34 @@ body {
 </style>
 </head>
 <body>
-${projectName && HTML`
-  <div class="edit-link"><a href="https://glitch.com/edit/#!/${projectName}?path=${req.url.slice(1)}">edit on Glitch</a></div>
+${projectName &&
+  HTML`
+  <div class="edit-link"><a href="https://glitch.com/edit/#!/${projectName}?path=${req.url.slice(
+    1,
+  )}">edit on Glitch</a></div>
 `}
 <main>
   ${HTML(result)}
 </main>
 </body>
-</html>`);
+</html>`,
+  );
 });
 
-app.get('/favicon.ico', (req, res) => { res.sendFile(__dirname + '/src/icon.png'); });
+app.get("/favicon.ico", (req, res) => {
+  res.sendFile(__dirname + "/src/icon.png");
+});
 
-
-import {SqliteStringMap} from './sqlite-string-map.js';
+import { SqliteStringMap } from "./sqlite-string-map.js";
 
 // Crudely mirror and cache speedrun.com/api.
 // We never expire/evict values here; we assume the
 // process won't live long enough for it to matter.
-const apiCache = new SqliteStringMap('api-cache');
+const apiCache = new SqliteStringMap("api-cache");
 // apiCache.clear();
 
 app.get(/^\/api\/(.*)/, async (req, res) => {
-  const url = 'https://www.speedrun.com/' + req.url.slice(1);
+  const url = "https://www.speedrun.com/" + req.url.slice(1);
   const cached = await apiCache.get(url);
 
   if (cached) {
@@ -108,82 +119,108 @@ app.get(/^\/api\/(.*)/, async (req, res) => {
 
   console.log("cache MISS", url);
 
-  const result = rp.get(url, {simple: false});
-  
+  const result = rp.get(url, { simple: false });
+
   // We don't await/block on the result of the set.
   apiCache.set(url, result);
 
   return res.json(JSON.parse(await result));
 });
 
-import {BestsRouter} from '/assets/router.js';
-import {Header, Footer} from '/assets/common.js';
-import fs from 'fs';
-import recursiveReaddir from 'recursive-readdir';
+import { BestsRouter } from "/assets/router.js";
+import { Header, Footer } from "/assets/common.js";
+import fs from "fs";
+import recursiveReaddir from "recursive-readdir";
 
 let bodyCache = {};
 app.use(async (req, res) => {
-  const index = await new Promise((resolve, reject) => fs.readFile(__dirname + '/src/index.html', 'utf8', (err, data) => { err ? reject(err) : resolve(data); }));
-  
+  console.log("got request", req);
+  const index = await new Promise((resolve, reject) =>
+    fs.readFile(__dirname + "/src/index.html", "utf8", (err, data) => {
+      err ? reject(err) : resolve(data);
+    }),
+  );
+
   const [gamesSlug, runnerSlug] = req.url.slice(1).split(/\//g);
   const gameSlugs = gamesSlug.split(/\+/g).filter(Boolean);
 
-  let body = '';
+  let body = "";
   const state = [];
   if (bodyCache[req.path]) {
-    state.push('loaded');
+    state.push("loaded");
     body = bodyCache[req.path];
   } else {
     try {
       body = await Promise.race([
         (async () => {
           const result = await HTML.string`<div>
-            ${BestsRouter.of({url: new url.URL(req.path, `https://${req.hostname}/`)})}
+            ${BestsRouter.of({
+              url: new url.URL(req.path, `http://${req.hostname}/`),
+            })}
           </div>`;
           bodyCache[req.path] = result;
-          state.push('loaded');
+          state.push("loaded");
           return result;
         })(),
         (async () => {
           // time-out server side render at 1s, but all required fetches will continue in background.
           await new Promise(resolve => setTimeout(resolve, 125));
           const result = await HTML.string`<div>
-            ${Header.of({currentHost: req.get('host'), currentProject: process.env.PROJECT_NAME})}
+            ${Header.of({
+              currentHost: req.get("host"),
+              currentProject: process.env.PROJECT_NAME,
+            })}
             <p>
               Still loading data from speedrun.com. Please try again in a minute.
             </p>
             ${Footer.of()}
           </div>`;
-          state.push('errored');
+          state.push("errored");
           return result;
         })(),
       ]);
     } catch (error) {
-      state.push('errored');
+      state.push("errored");
       body = await HTML.string`<div>
-            ${Header.of({currentHost: req.get('host'), currentProject: process.env.PROJECT_NAME})}
+            ${Header.of({
+              currentHost: req.get("host"),
+              currentProject: process.env.PROJECT_NAME,
+            })}
             <pre>${error}\n${error.stack}</pre>
             ${Footer.of()}
           </div>`;
     }
   }
-  if (state[0] === 'loaded') {
+  if (state[0] === "loaded") {
     // res.status(200); // full response
-  } else if (state[0] === 'errored') {
+  } else if (state[0] === "errored") {
     // res.status(500); // maybe-persistent error
   } else {
     // res.status(504); // still 'loading' - gateway timeout
   }
-  res.set('Content-Type', 'text/html');
-  return res.send(index
-                  .replace('unloaded', state[0])
-                  .replace(`import '/assets/main.js';`, await recursiveReaddir('src').then(files => files.filter(s => /\.js$/i.test(s) && !s.includes('-node') ).map(s => `import ${JSON.stringify(s.replace(/^src\//, '/assets/'))};`).sort().join('\n')))
-                  .replace('</main>', body + '</main>'));
+  res.set("Content-Type", "text/html");
+  return res.send(
+    index
+      .replace("unloaded", state[0])
+      .replace(
+        `import '/assets/main.js';`,
+        await recursiveReaddir("src").then(files =>
+          files
+            .filter(s => /\.js$/i.test(s) && !s.includes("-node"))
+            .map(
+              s => `import ${JSON.stringify(s.replace(/^src\//, "/assets/"))};`,
+            )
+            .sort()
+            .join("\n"),
+        ),
+      )
+      .replace("</main>", body + "</main>"),
+  );
 });
 
 // Serve index for unknown URLs so it can route them client-side.
 app.use((req, res) => {
-  res.sendFile(__dirname + '/src/index.html');
+  res.sendFile(__dirname + "/src/index.html");
 });
 
 const listener = app.listen(3002, () => {
