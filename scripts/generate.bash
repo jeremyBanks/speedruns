5ac9793489c17bb7eeaf23581d9118045e08a80e
Jeremy <_@jeremy.ca>
2020-01-14 01:53:04 +0000

    r882 charlie zero six zero

--- scripts/generate.bash
+++ scripts/generate.bash
@@ -13,7 +13,7 @@ apollo client:codegen --target typescript --outputFlat src/pages-lib/schema.ts
 apollo client:download-schema public/graphql/schema.json
 
 # ignored files
-graphql-docs-gen http://localhost:3001/ public/graphql/schema.html
+graphql-docs-gen http://localhost:3001/graphql public/graphql/schema.html
 apollo client:download-schema public/graphql/schema.apollo.graphql
 cp -f node_modules/graphql-voyager/dist/voyager.worker.js public/graphql/voyager.worker.js
 cp -f node_modules/graphql-voyager/dist/voyager.css public/graphql/voyager.css
