c79f2af620502d2c9f28a280860f495a1f541440
Jeremy <_@jeremy.ca>
2020-01-16 01:27:28 +0000

    r895 bravo seven seven delta

--- src/pages-lib/schema.ts
+++ src/pages-lib/schema.ts
@@ -54,10 +54,52 @@ export interface GetGamePage_game_categories {
   leaderboard: GetGamePage_game_categories_leaderboard[];
 }
 
+export interface GetGamePage_game_levels_leaderboard_run_category {
+  __typename: "Category";
+  id: string;
+}
+
+export interface GetGamePage_game_levels_leaderboard_run_level {
+  __typename: "Level";
+  id: string;
+}
+
+export interface GetGamePage_game_levels_leaderboard_run_players_user {
+  __typename: "User";
+  id: string;
+  slug: string;
+}
+
+export interface GetGamePage_game_levels_leaderboard_run_players {
+  __typename: "Player";
+  name: string;
+  isGuest: boolean;
+  user: GetGamePage_game_levels_leaderboard_run_players_user | null;
+}
+
+export interface GetGamePage_game_levels_leaderboard_run {
+  __typename: "Run";
+  id: string;
+  category: GetGamePage_game_levels_leaderboard_run_category;
+  level: GetGamePage_game_levels_leaderboard_run_level | null;
+  date: number | null;
+  players: GetGamePage_game_levels_leaderboard_run_players[];
+}
+
+export interface GetGamePage_game_levels_leaderboard {
+  __typename: "LeaderboardRun";
+  rank: number;
+  timeMs: number;
+  isTied: boolean;
+  tiedRank: number;
+  run: GetGamePage_game_levels_leaderboard_run;
+}
+
 export interface GetGamePage_game_levels {
   __typename: "Level";
   id: string;
   name: string;
+  leaderboard: GetGamePage_game_levels_leaderboard[];
 }
 
 export interface GetGamePage_game {
@@ -118,6 +160,55 @@ export interface GetUser {
 
 export interface GetUserVariables {
   slug: string;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: GameLeaderboardRun
+// ====================================================
+
+export interface GameLeaderboardRun_run_category {
+  __typename: "Category";
+  id: string;
+}
+
+export interface GameLeaderboardRun_run_level {
+  __typename: "Level";
+  id: string;
+}
+
+export interface GameLeaderboardRun_run_players_user {
+  __typename: "User";
+  id: string;
+  slug: string;
+}
+
+export interface GameLeaderboardRun_run_players {
+  __typename: "Player";
+  name: string;
+  isGuest: boolean;
+  user: GameLeaderboardRun_run_players_user | null;
+}
+
+export interface GameLeaderboardRun_run {
+  __typename: "Run";
+  id: string;
+  category: GameLeaderboardRun_run_category;
+  level: GameLeaderboardRun_run_level | null;
+  date: number | null;
+  players: GameLeaderboardRun_run_players[];
+}
+
+export interface GameLeaderboardRun {
+  __typename: "LeaderboardRun";
+  rank: number;
+  timeMs: number;
+  isTied: boolean;
+  tiedRank: number;
+  run: GameLeaderboardRun_run;
 }
 
 /* tslint:disable */
--- src/pages/[game]/index.tsx
+++ src/pages/[game]/index.tsx
@@ -32,7 +32,7 @@ const GamePage: NextPage = () => {
       <h1>{game.name}</h1>
 
       {game.categories.map(category => (
-        <>
+        <div key={category.id}>
           <h2>{category.name}</h2>
 
           <h3>Leaderboard</h3>
@@ -47,7 +47,54 @@ const GamePage: NextPage = () => {
             <tbody>
               {category.leaderboard.map(ranked => {
                 return (
-                  <tr data-rank={ranked.tiedRank}>
+                  <tr key={ranked.run.id} data-rank={ranked.tiedRank}>
+                    <td className={styles.rank}>{ranked.tiedRank}</td>
+                    <td className={styles.player}>
+                      <AutoColor>
+                        {ranked.run.players.map(p => p.name).join(" & ")}
+                      </AutoColor>
+                    </td>
+                    <td className={styles.time}>
+                      <Duration ms={ranked.timeMs} />
+                    </td>
+                    <td className={styles.date}>
+                      <AutoColor>
+                        {String(
+                          (ranked.run.date &&
+                            new Date(ranked.run.date * 1000)
+                              .toISOString()
+                              .slice(0, "YYYY-MM-DD".length)) ||
+                            "",
+                        )}
+                      </AutoColor>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ))}
+
+      <h2>Individual Levels</h2>
+
+      {game.levels.map(level => (
+        <>
+          <h3>{level.name}</h3>
+
+          <h4>Leaderboard</h4>
+
+          <table className={styles.leaderboard}>
+            <thead>
+              <th className={styles.rank}>Rank</th>
+              <th className={styles.player}>Player</th>
+              <th className={styles.time}>Time (RTA)</th>
+              <th className={styles.date}>Date</th>
+            </thead>
+            <tbody>
+              {level.leaderboard.map(ranked => {
+                return (
+                  <tr key={ranked.run.id} data-rank={ranked.tiedRank}>
                     <td className={styles.rank}>{ranked.tiedRank}</td>
                     <td className={styles.player}>
                       <AutoColor>
@@ -75,15 +122,42 @@ const GamePage: NextPage = () => {
           </table>
         </>
       ))}
-
-      <h2>Individual Levels</h2>
     </section>
   );
 };
 
 export default withApollo(GamePage);
 
+const GameLeaderboardRun = gql`
+  fragment GameLeaderboardRun on LeaderboardRun {
+    rank
+    timeMs
+    isTied
+    tiedRank
+    run {
+      id
+      category {
+        id
+      }
+      level {
+        id
+      }
+      date
+      players {
+        name
+        isGuest
+        user {
+          id
+          slug
+        }
+      }
+    }
+  }
+`;
+
 const GetGamePage = gql`
+  ${GameLeaderboardRun}
+
   query GetGamePage($slug: String!) {
     game: game(slug: $slug) {
       id
@@ -92,35 +166,16 @@ const GetGamePage = gql`
       categories {
         id
         name
-
         leaderboard {
-          rank
-          timeMs
-          isTied
-          tiedRank
-          run {
-            id
-            category {
-              id
-            }
-            level {
-              id
-            }
-            date
-            players {
-              name
-              isGuest
-              user {
-                id
-                slug
-              }
-            }
-          }
+          ...GameLeaderboardRun
         }
       }
       levels {
         id
         name
+        leaderboard(categorySlug: "mission") {
+          ...GameLeaderboardRun
+        }
       }
     }
   }
