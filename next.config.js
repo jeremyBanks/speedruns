a650ebf78c195dd6b987c564babed33fd3b161c0
Jeremy Banks <_@jeremy.ca>
2020-01-16 21:56:12 -0500

    move mockup into game page

--- next.config.js
+++ next.config.js
@@ -9,12 +9,8 @@ module.exports = [
     return {
       "/": { page: "/" },
       "/graphql/voyager": { page: "/graphql/voyager" },
-      "/mockup": { page: "/mockup" },
-      "/sc1": { page: "/[game]", query: { game: "sc1" } },
-      "/scbw": { page: "/[game]", query: { game: "scbw" } },
       "/wc2": { page: "/[game]", query: { game: "wc2" } },
       "/wc2btdp": { page: "/[game]", query: { game: "wc2btdp" } },
-      "/celeste": { page: "/[game]", query: { game: "celeste" } },
       "/user/zpr": { page: "/user/[user]", query: { user: "zpr" } },
       "/user/banks": { page: "/user/[user]", query: { user: "banks" } },
       "/user/szwagier": { page: "/user/[user]", query: { user: "szwagier" } },
