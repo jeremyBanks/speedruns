b7d9afd429a8cb686e2093408172a13ade9e7cd2
Jeremy <_@jeremy.ca>
2020-01-18 02:31:28 +0000

    r914 four nine two six

--- src/pages-lib/home.tsx
+++ src/pages-lib/home.tsx
@@ -5,15 +5,6 @@ import styles from "./styles.module.scss";
 
 export const HomeContent: React.FC = () => (
   <div className={styles.home}>
-    <p>
-      WARNING: this is currently using a test dataset; the leadboards are not
-      accurate or complete.
-    </p>
-
-    <p>
-      This site compares personal and world record speed run progress over time.
-    </p>
-
     <ul>
       <li>
         <Link href="/wc2">
