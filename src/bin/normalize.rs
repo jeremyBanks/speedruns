023bfa7b7f1741ca3d1d56eb1b19ebd193f46064
Jeremy Banks <_@jeremy.ca>
2020-01-17 20:10:48 -0500

    tell clippy to complain about unwrapping options and results

--- src/bin/normalize.rs
+++ src/bin/normalize.rs
@@ -4,6 +4,8 @@
     clippy::cognitive_complexity,
     clippy::clone_on_copy
 )]
+#![warn(clippy::option_unwrap_used, clippy::result_unwrap_used)]
+
 use std::{
     collections::HashSet,
     fs::File,
