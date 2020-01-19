0210b9eaa345669470a02cfe54819e5d4d9399d4
Jeremy Banks <_@jeremy.ca>
2020-01-19 00:11:27 -0500

    nicer ids

--- src/data/graphql/global_id.rs
+++ src/data/graphql/global_id.rs
@@ -17,12 +17,13 @@ pub fn global_id(id: u64, node_type: NodeType) -> ID {
     assert!(bytes[1] == 0, "second-high byte of id must be zero");
 
     bytes[0] = match node_type {
-        Game => 0x18,
-        User => 0x50,
-        Run => 0x44,
-        Category => 0x08,
-        Level => 0x2C,
+        Game => 0x83,
+        User => 0xBB,
+        Run => 0xAF,
+        Category => 0x73,
+        Level => 0x97,
     };
+    bytes[1] |= 0xE0;
 
     ID::from(base64::encode_config(&bytes, base64::URL_SAFE_NO_PAD))
 }
@@ -31,18 +32,19 @@ pub fn global_id(id: u64, node_type: NodeType) -> ID {
 pub fn parse_global_id(global_id: &juniper::ID) -> Result<(u64, NodeType), !> {
     let mut bytes = base64::decode_config(&global_id.to_string(), base64::URL_SAFE_NO_PAD)
         .expect("infallible");
-    assert!(bytes[1] == 0, "second-high byte must be zero");
+    assert!(bytes[1] == 0xE0, "second-high byte must be 0xE0");
 
     let node_type = match bytes[0] {
-        0x18 => Game,
-        0x50 => User,
-        0x44 => Run,
-        0x08 => Category,
-        0x2C => Level,
+        0x83 => Game,
+        0xBB => User,
+        0xAF => Run,
+        0x73 => Category,
+        0x97 => Level,
         _ => panic!("high byte didn't match expected tag values"),
     };
 
     bytes[0] = 0;
+    bytes[1] &= 0x0F;
 
     let mut bytes_array = [0u8; 8];
     let bytes = &bytes[..bytes_array.len()];
@@ -53,12 +55,19 @@ pub fn parse_global_id(global_id: &juniper::ID) -> Result<(u64, NodeType), !> {
 
 #[test]
 fn test_round_trip_global_ids() {
+    use crate::utils::u64_from_base36;
+
     let cases = [
-        (0x1234u64, NodeType::Game, "GAAAAAAAEjQ"),
-        (0x0000_FFFF_FFFF_FFFF, NodeType::User, "UAD_______8"),
-        (0x0000_FEDA_BCAC_EDAC, NodeType::Run, "RAD-2rys7aw"),
-        (0x0, NodeType::Category, "CAAAAAAAAAA"),
-        (0x1, NodeType::Level, "LAAAAAAAAAE"),
+        (0x1234u64, NodeType::Game, "g-AAAAAAEjQ"),
+        (0x0000_FFFF_FFFF_FFFF, NodeType::User, "u-D_______8"),
+        (0x0000_FEDA_BCAC_EDAC, NodeType::Run, "r-D-2rys7aw"),
+        (0x0, NodeType::Category, "c-AAAAAAAAA"),
+        (0x1, NodeType::Level, "l-AAAAAAAAE"),
+        (
+            u64_from_base36("zzzzzzzz").expect("it's valid"),
+            NodeType::Level,
+            "l-ACkNdA__8",
+        ),
     ];
 
     for (id, node_type, global) in &cases {
