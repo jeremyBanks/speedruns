a0a0646746ba660a76e594899af893a385974da3
Jeremy Banks <_@jeremy.ca>
2020-01-23 00:54:12 -0500

    down

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #[allow(unused)]
 use juniper::{
-    graphql_interface, graphql_object, graphql_scalar, graphql_union, graphql_value,
-    object, GraphQLEnum, GraphQLInputObject, GraphQLObject, GraphQLScalarValue,
-    ScalarValue,
+    graphql_interface, graphql_object, graphql_scalar, graphql_union, graphql_value, object,
+    GraphQLEnum, GraphQLInputObject, GraphQLObject, GraphQLScalarValue, ScalarValue,
 };
 use juniper::{Executor, ID};
 use juniper_from_schema::graphql_schema_from_file;
@@ -10,7 +9,7 @@ use juniper_from_schema::graphql_schema_from_file;
 graphql_schema_from_file!("src/schema.graphql");
 
 pub fn schema() -> Schema {
-    Schema::new(Speedruns {}, Speedruns {})
+    Schema::new(Query {}, Query {})
 }
 
 #[derive(Debug, Clone)]
@@ -19,66 +18,63 @@ pub struct Context;
 impl juniper::Context for Context {}
 
 #[derive(Debug)]
-pub struct Speedruns;
+pub struct Query;
 
 #[derive(Debug, Clone)]
-pub struct Game;
+pub struct Grandparent;
 
 #[derive(Debug, Clone)]
-pub struct Run;
+pub struct Grandchild;
 
 #[derive(Debug, Clone)]
-pub struct LeaderboardRun;
+pub struct Child;
 
 #[derive(Debug, Clone)]
-pub struct Category;
+pub struct Parent;
 
-impl SpeedrunsFields for Speedruns {
-    fn field_game(
+impl QueryFields for Query {
+    fn field_grandparent(
         &self,
         _executor: &Executor<'_, Context>,
-        _trail: &QueryTrail<'_, Game, Walked>,
-        _slug: String,
-    ) -> Option<Game> {
-        Some(Game)
+        _trail: &QueryTrail<'_, Grandparent, Walked>,
+    ) -> Option<Grandparent> {
+        Some(Grandparent)
     }
 }
 
-impl GameFields for Game {
-    fn field_game_categories(
+impl GrandparentFields for Grandparent {
+    fn field_parent(
         &self,
         _executor: &Executor<'_, Context>,
-        _trail: &QueryTrail<'_, Category, Walked>,
-    ) -> Vec<Category> {
-        vec![Category]
+        _trail: &QueryTrail<'_, Parent, Walked>,
+    ) -> Vec<Parent> {
+        vec![Parent]
     }
 }
 
-impl RunFields for Run {
+impl ParentFields for Parent {
+    fn field_child(
+        &self,
+        _executor: &Executor<'_, Context>,
+        _trail: &QueryTrail<'_, Child, Walked>,
+    ) -> Vec<Child> {
+        vec![Child]
+    }
+}
+
+impl ChildFields for Child {
+    fn field_grandchild(
+        &self,
+        _executor: &Executor<'_, Context>,
+        _trail: &QueryTrail<'_, Grandchild, Walked>,
+    ) -> Grandchild {
+        Grandchild
+    }
+}
+
+impl GrandchildFields for Grandchild {
     fn field_id(&self, _executor: &Executor<'_, Context>) -> ID {
         "example-id".to_string().into()
-    }
-}
-
-impl LeaderboardRunFields for LeaderboardRun {
-    fn field_run(
-        &self,
-        _executor: &Executor<'_, Context>,
-        _trail: &QueryTrail<'_, Run, Walked>,
-    ) -> Run {
-        Run
-    }
-}
-
-impl CategoryFields for Category {
-    fn field_leaderboard(
-        &self,
-        _executor: &Executor<'_, Context>,
-        _trail: &QueryTrail<'_, LeaderboardRun, Walked>,
-        _level_slug: Option<String>,
-        _include_obsolete: bool,
-    ) -> Vec<LeaderboardRun> {
-        vec![LeaderboardRun]
     }
 }
 
@@ -86,31 +82,25 @@ fn main() {
     let schema = schema();
 
     let query = r#"
-        fragment GameRun on Run {
+        fragment GrandchildFragment on Grandchild {
             id
         }
-        fragment GameLeaderboardRun on LeaderboardRun {
-            run {
-                ...GameRun
+
+        fragment ChildFragment on Child {
+            grandchild {
+                ...GrandchildFragment
             }
         }
-        query GetGamePage {
-            game: game(slug: "wc2") {
-                gameCategories {
-                    leaderboard {
-                        ...GameLeaderboardRun
+        query {
+            grandparent {
+                parent {
+                    child {
+                        ...ChildFragment
                     }
                 }
             }
         }
     "#;
 
-    juniper::execute(
-        query,
-        None,
-        &schema,
-        &juniper::Variables::new(),
-        &Context,
-    )
-    .unwrap();
+    juniper::execute(query, None, &schema, &juniper::Variables::new(), &Context).unwrap();
 }
