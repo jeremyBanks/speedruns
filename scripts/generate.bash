02177142ccfca2e6c74750b47a685fd71f9a5a4e
Jeremy Banks <_@jeremy.ca>
2020-01-30 22:28:19 -0500

    suplemental data requires fixture data at minimum
    this is probably a horrible practice.

--- scripts/generate.bash
+++ scripts/generate.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euxo pipefail
 
+if [ ! -f data/normalized/runs.jsonl ]; then
+    echo "WARNING: no data found, so loading minimal test/fixture data"
+    git checkout 48dc6d44639549ff9ebf70faba4a39956031f4fb data/normalized/{categories,games,levels,runs,users}.jsonl
+    git reset HEAD data/normalized/{categories,games,levels,runs,users}.jsonl
+fi
+
 cargo build --release --bin serve
 
 cargo run --release --bin serve &
