95ca0a3bd3ba99d7da4ecf1fb6f957f8a69c1363
Jeremy Banks <_@jeremy.ca>
2020-02-01 01:17:50 -0500

    load fixtures in scripts/export.bash

--- scripts/export.bash
+++ scripts/export.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
+if [ ! -f data/normalized/runs.jsonl ]; then
+    echo "WARNING: no data found, so loading minimal test/fixture data"
+    cp data/fixture/{categories,games,levels,runs,users}.jsonl data/normalized/
+fi
+
 cargo build --release --workspace
 
 cargo run --release serve &
