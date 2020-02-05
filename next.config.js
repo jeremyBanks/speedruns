0c1f3b36073519213ade95afd80ad734386f4d20
Jeremy Banks <_@jeremy.ca>
2020-02-04 22:36:14 -0500

    Automatically publish dev Node releases to NPM and GitHub packages from stable.

--- next.config.js
+++ next.config.js
@@ -52,10 +52,6 @@ module.exports = [
         test: /\.(png|svg|jpg|gif|jpeg)$/,
         use: ["file-loader"],
       },
-      {
-        test: /\.toml$/,
-        use: ["@lcdev/toml-loader"],
-      },
     );
     config.resolve.alias["~"] = path.resolve("./src");
     return config;
