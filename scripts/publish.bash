6cf7130e26e80f1cafa88597d91eed04a8d6aee3
Jeremy <_@jeremy.ca>
2020-02-05 04:43:44 +0000

    r1191 six zero echo delta

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -5,23 +5,14 @@ echo "//npm.pkg.github.com/:_authToken=${GITHUB_PUBLISH_TOKEN}" > .npmrc
 echo "@jeremybanks:registry=https://npm.pkg.github.com" >> .npmrc
 echo "always-auth=true" >> .npmrc
 
-npm version prerelease --no-git-tag-version --preid="r$(git rev-list --first-parent HEAD | wc -l)-$(git rev-parse --short=4 HEAD)"
+npm version prerelease --no-git-tag-version --preid="r$(git rev-list --first-parent HEAD | wc -l)-$(git rev-parse --short=6 HEAD)"
 
-NODE_AUTH_TOKEN=${GITHUB_PUBLISH_TOKEN} npm --registry=https://npm.pkg.github.com/:_authToken=${GITHUB_PUBLISH_TOKEN} publish
-NODE_AUTH_TOKEN=${NPM_PUBLISH_TOKEN} npm --registry=https://registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN} publish
-
-sed -i 's/@jeremybanks\///' package.json
-
-NODE_AUTH_TOKEN=${NPM_PUBLISH_TOKEN} npm --registry=https://registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN} publish
-
-npm --registry=https://npm.pkg.github.com/:_authToken=${GITHUB_PUBLISH_TOKEN} publish
-
-rm .npmrc
+npm --registry=https://npm.pkg.github.com/ publish
 
 echo "//registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN}" > .npmrc
 
-npm --registry=https://registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN} publish
+npm --registry=https://registry.npmjs.org/ publish
 
 sed -i 's/@jeremybanks\///' package.json
 
-npm --registry=https://registry.npmjs.org/:_authToken=${NPM_PUBLISH_TOKEN} publish
+npm --registry=https://registry.npmjs.org/ publish
