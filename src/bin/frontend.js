aa6ac7a73a386b72c31a44715b8bc3d535bb3573
Jeremy Banks <_@jeremy.ca>
2020-02-07 22:02:19 -0500

    executable?

--- src/bin/frontend.js
+++ src/bin/frontend.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+"use strict";
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev: dev });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
