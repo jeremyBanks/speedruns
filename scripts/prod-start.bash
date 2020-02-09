44f9e075871bd84c95a7af820aa8b296fe01a1aa
Jeremy Banks <_@jeremy.ca>
2020-02-09 15:20:09 -0500

    ;

--- scripts/prod-start.bash
+++ scripts/prod-start.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -vexuo pipefail;
+set -vexuo pipefail
 
-source ~/.bashrc;
-source $HOME/.cargo/env;
-cd ~/speedruns;
+source ~/.bashrc
+source $HOME/.cargo/env
+cd ~/speedruns
 
-export PATH="$PATH:/home/speedrun/.nvm/versions/node/v13.5.0/bin/";
+export PATH="$PATH:/home/speedrun/.nvm/versions/node/v13.5.0/bin/"
 
-npm install -g --force speedruns@^0.20.20-dev.0;
+npm install -g --force speedruns@^0.20.20-dev.0
 version="$(speedruns-frontend --version)"
 
 curl -L -o speedruns-linux-x86_64 https://github.com/jeremyBanks/speedruns/releases/download/$version/speedruns-linux-x86_64
 chmod +x speedruns-linux-x86_64
 
-GRAPHQL_ENDPOINT=http://localhost:3001/graphql speedruns-frontend &;
+GRAPHQL_ENDPOINT=http://localhost:3001/graphql speedruns-frontend &
 ./speedruns-linux-x86_64 serve
