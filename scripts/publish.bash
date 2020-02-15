1398100dfb5588790b57feb7683645ca1143868f
Jeremy <_@jeremy.ca>
2020-02-15 18:35:44 +0000

    r1377 delta two delta alfa

--- scripts/publish.bash
+++ scripts/publish.bash
@@ -45,8 +45,8 @@ git diff
 
 npm --registry=https://registry.npmjs.org/ publish $publish_args
 
-(cd src/lib/utils && cargo publish $publish_args --no-verify --token "$CARGO_PUBLISH_TOKEN" --allow-dirty && cd -)
-cargo publish $publish_args --no-verify --token "$CARGO_PUBLISH_TOKEN" --allow-dirty
+cargo install cargo-publish-all
+cargo-publish-all $publish_args --no-verify --token "$CARGO_PUBLISH_TOKEN" --allow-dirty
 
 rm .npmrc
 git checkout HEAD package.json Cargo.toml Cargo.lock
