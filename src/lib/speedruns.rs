dcc26ffe9657e50831dc236f5d37f16deb2664c8
Jeremy Banks <_@jeremy.ca>
2020-02-22 13:30:11 -0500

    beauty

--- src/lib/speedruns.rs
+++ src/lib/speedruns.rs
@@ -2,6 +2,9 @@
 #![allow(missing_docs, clippy::useless_attribute, clippy::useless_vec)]
 #![warn(missing_debug_implementations)]
 
+#[macro_use]
+extern crate rental;
+
 /// Types for the speedrun.com API data we consume, and utilities for normalizing it.  
 pub mod api;
 
@@ -11,13 +14,72 @@ pub mod api;
 pub mod data;
 
 /// The core types of our data model.
-pub mod types {
-    pub use super::data::types::*;
-}
+pub mod types;
 
 /// Validating, indexing, and serializing our data.
 pub mod database {
-    pub use super::data::database::*;
+    use std::collections::{BTreeMap as SortedMap, HashMap};
+    use std::sync::Arc;
+
+    use crate::types::{Category, Game, Level, Run, User};
+
+    #[derive(Debug, Clone)]
+    pub struct Tables {
+        games: HashMap<u64, Game>,
+        categories: HashMap<u64, Category>,
+        runs: HashMap<u64, Run>,
+        users: HashMap<u64, User>,
+        levels: HashMap<u64, Level>,
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Indicies<'tables> {
+        tables: &'tables Tables,
+        games_by_slug: SortedMap<String, &'tables Game>,
+    }
+
+    impl<'tables> Indicies<'tables> {
+        pub fn from_tables<'a>(tables: &'a Tables) -> Indicies<'a> {
+            Indicies {
+                tables,
+                games_by_slug: tables
+                    .games
+                    .values()
+                    .map(|game| (game.slug.clone(), game))
+                    .collect(),
+            }
+        }
+    }
+
+    rental! {
+        mod rentals {
+            use super::*;
+            #[rental(debug, clone, covariant)]
+            pub struct Database {
+                tables: Arc<Tables>,
+                indicies: Indicies<'tables>,
+            }
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Database(rentals::Database);
+
+    impl Database {
+        pub fn new(tables: Arc<Tables>) -> Database {
+            Database(rentals::Database::new(tables, |tables| {
+                Indicies::from_tables(tables)
+            }))
+        }
+
+        pub fn tables(&self) -> &Tables {
+            self.0.head()
+        }
+
+        pub fn indicies(&self) -> &Indicies {
+            self.0.suffix()
+        }
+    }
 }
 
 /// Functions for leaderboards, progression, etc.
@@ -29,7 +91,4 @@ pub mod aggregation {
 /// Utilities that should probably go somewhere more specific.
 pub use speedruns_utils as utils;
 
-pub use crate::data::{
-    database::Database,
-    types::*,
-};
+pub use crate::data::{database::Database, types::*};
