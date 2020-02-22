255b6150f5464ae2b6e8fd6cc084d2eef358c850
Jeremy <_@jeremy.ca>
2020-02-22 17:08:16 +0000

    r1447 bravo seven charlie three

--- src/lib/speedruns.rs
+++ src/lib/speedruns.rs
@@ -2,16 +2,34 @@
 #![allow(missing_docs, clippy::useless_attribute, clippy::useless_vec)]
 #![warn(missing_debug_implementations)]
 
-/// Types for the speedrun.com API data we consume and utilities for normalizing it.  
+/// Types for the speedrun.com API data we consume, and utilities for normalizing it.  
 pub mod api;
 
 /// Our normalized data types, a frozen in-memory database, and leaderboard logic.
+///
+/// TODO: refactor this out of existence
 pub mod data;
+
+/// The core types of our data model.
+pub mod types {
+    pub use super::data::types::*;
+}
+
+/// Validating, indexing, and serializing our data.
+pub mod database {
+    pub use super::data::database::*;
+}
+
+/// Functions for leaderboards, progression, etc.
+pub mod aggregation {
+    pub use super::data::leaderboard;
+    pub use super::data::progression;
+}
 
 /// Utilities that should probably go somewhere more specific.
 pub use speedruns_utils as utils;
 
 pub use crate::data::{
     database::Database,
-    types::{self, *},
+    types::*,
 };
